3339f0532088455bba94a8a2d2e8e4f9a67942ec
elenanope <elenaromanmoreno@gmail.com>
2024-11-19 21:39:19 +0100

    fuentes, trigger hombre y solucion iluminacion

--- No Secrets/Assets/SHHHH/Scripts/TriggerManCake.cs
+++ No Secrets/Assets/SHHHH/Scripts/TriggerManCake.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class TriggerManCake : MonoBehaviour
 {
+
+    public GameObject dialoguito;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Cake"))
         {
             Debug.Log("Cómo? me estaba haciendo una fiesta?!");
+
             
         }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dialoguito.SetActive(true);
 
+
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dialoguito.SetActive(false);
+
+
+        }
     }
 
 }
